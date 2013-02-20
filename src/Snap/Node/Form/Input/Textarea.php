afda9950c9a548c92136c94fbfc8ad51c0734610
bheilman <bheilman@estenda.com>
2013-02-20 11:27:56 -0500

    Making it be able to accept base Models

--- src/Snap/Node/Form/Input/Textarea.php
+++ src/Snap/Node/Form/Input/Textarea.php
@@ -8,10 +8,10 @@ class Textarea extends \Snap\Node\Form\Input {
 		$rows, 
 		$cols;
 	
-	public function __construct( $settings = array() ){
+	protected function parseSettings( $settings = array() ){
 		$settings['tag'] = 'textarea';
 		
-		parent::__construct( $settings );
+		parent::parseSettings( $settings );
 
 		$this->rows = isset($settings['rows']) ? $settings['rows'] : null;
 		$this->cols = isset($settings['cols']) ? $settings['cols'] : null;
