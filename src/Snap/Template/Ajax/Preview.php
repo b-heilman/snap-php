a49338883c0b5ecd2a33e39cc845e2a3ecda5cf1
bheilman <bheilman@estenda.com>
2013-04-05 12:03:36 -0400

    blah

--- src/Snap/Template/Ajax/Preview.php
+++ src/Snap/Template/Ajax/Preview.php
@@ -1,7 +1,7 @@
 <div class="preview-wrapper">
 	<div class='preview-list'><?php $this->append( $this->linkList ); ?></div>
 	<div class='preview-pane'>
-		<span class='preview-back'>Back</span>
+		<span class='preview-back-wrapper'><a class='preview-back link' href="javascript:void(null);">Back</a></span>
 		<div class='preview-content'></div>
 	</div>
 </div>
